04bfb14f20e857592461cfb25ab9ce69a09ad4bf
kschopmeyer <k.schopmeyer@swbell.net>
2017-06-26 13:10:56 -0500

    go to clean version environment

--- smipyping/_version.py
+++ smipyping/_version.py
@@ -1,18 +1,43 @@
-# There are submodules, but clients shouldn't need to know about them.
-# Importing just this module is enough.
-# These are explicitly safe for 'import *'
+# Copyright 2016-2017 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#  Copyright 2016-2017 Karl Schopmeyer All Rights Reserved.
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
-Version of the smipyping package.
+Access to the package version, and check for supported Python versions.
+
+Note: The package version is not defined here, but determined dynamically by
+the `pbr` package from Git information.
 """
 
-#: Version of the smipyping package, as a :term:`string`.
+import sys
+import pbr.version
+
+__all__ = ['__version__']
+
+#: The full version of this package including any development levels, as a
+#: :term:`string`.
 #:
-#: Possible formats for the version string are:
+#: Possible formats for this version string are:
 #:
-#: * "M.N.U.dev0": During development of future M.N.U release (not released to
-#:   PyPI)
-#: * "M.N.U.rcX": Release candidate X of future M.N.U release (not released to
-#:   PyPI)
-#: * "M.N.U": The final M.N.U release
-__version__ = '0.6.0.dev0'
+#: * "M.N.P.devD": Development level D of a not yet released assumed M.N.P
+#:   version
+#: * "M.N.P": A released M.N.P version
+__version__ = pbr.version.VersionInfo('smipyping').release_string()
+
+# Check supported Python versions
+_PYTHON_M = sys.version_info[0]
+_PYTHON_N = sys.version_info[1]
+if _PYTHON_M == 2 and _PYTHON_N < 7:
+    raise RuntimeError('On Python 2, zhcmclient requires Python 2.7')
+elif _PYTHON_M == 3 and _PYTHON_N < 4:
+    raise RuntimeError('On Python 3, zhmcclient requires Python 3.4 or higher')
